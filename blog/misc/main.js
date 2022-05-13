93e554f02b8ef41e11e42324bdd7a378bee7b1f9
zethtren <houston.bova@gmail.com>
2022-05-13 15:58:04 -0400

    Working CORS Authenticated API request. Currently set to open but throttled

--- blog/misc/main.js
+++ blog/misc/main.js
@@ -1,17 +1,44 @@
 async function loadIntoTable(url, table) {
-    // Connect to HTML resources
-    const tableHead = table.querySelector("thead");
+    // Connect to HTML resources;
     const tableBody = table.querySelector("tbody");
     // Set CORS header to allow for REST API connection on Lambda
-    const response = await fetch(url, {
-        mode: 'cors'
-    });
-    // Read Data 
-    const data = await response.json();
-    // Temporarily log data to decide how to parse moving forward
-    console.log(data)
-    tableHead.innerHTML = "<tr></tr>";
-    tableBody.innerHTML = "";
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                //"Access-Control-Request-Method": "GET",
+                //"Access-Control-Request-Headers": "Content-Type, Accept",
+                //"Access-Control-Allow-Origin": "*"
+            }
+        });
+        try {
+            const data = await response.json();
+
+            for (item of data) {
+                const rowElement = document.createElement("tr");
+                const cellTitle = document.createElement("td");
+                cellTitle.textContent = item['title'];
+                rowElement.appendChild(cellTitle);
+                const cellDate = document.createElement("td");
+                cellDate.textContent = item['date'];
+                rowElement.appendChild(cellDate);
+                const cellPost = document.createElement("td");
+                cellPost.textContent = item['post'];
+                rowElement.appendChild(cellPost);
+
+                tableBody.appendChild(rowElement);
+            }
+            // for (item of data) {
+
+            // }
+        // Temporarily log data to decide how to parse moving forward
+        } catch (error) {
+            console.log("Error Reading Data");
+        }
+    } catch (error) {
+        console.log("Error Fetching Data");
+    }
 
 }
 let table = document.querySelector("table");
